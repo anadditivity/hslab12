d5fff5f0031bd6256062d5e471bf79def6bf3471
rnn00 <github.ed91r@simplelogin.co>
2024-05-24 13:22:38 +0300

    Kasutab nüüd Azure mikroteenuseid

--- apiparingud.js
+++ apiparingud.js
@@ -99,12 +99,12 @@ async function handleFormSubmit(event) {
 }
 
 async function listiraamatud() {
-    const responseData = await getDataAsJson("http://localhost:5000/raamatud/");
+    const responseData = await getDataAsJson("https://talvikhslab12.azurewebsites.net/raamatud/");
     const resultElement = document.getElementById("raamatud_result");
     resultElement.innerHTML = "";
     for (var raamat of responseData.raamatud){
-        resultElement.innerHTML += '<a href="http://localhost:5000/raamatud/'+raamat+'"  download="'+raamat+'" >' +raamat+".txt</a> " +
-                '<a href="#" onclick="deleteObject(\'http://localhost:5000/raamatud/'+raamat+'\')" > [kustuta]</a>' +
+        resultElement.innerHTML += '<a href="https://talvikhslab12.azurewebsites.net/raamatud/'+raamat+'"  download="'+raamat+'" >' +raamat+".txt</a> " +
+                '<a href="#" onclick="deleteObject(\'https://talvikhslab12.azurewebsites.net/raamatud/'+raamat+'\')" > [kustuta]</a>' +
                 "<br />";
     }
 
